3aa6410f231a8f5aab689d78292eb97e78ae8ea4
Salim Ramirez <salimramirezm@gmail.com>
2025-12-31 23:23:15 -0500

    chore: initial commit

---
+++ src/main/java/com/salimramirez/civabusapi/CivaBusApiApplication.java
@@ -0,0 +1,13 @@
+package com.salimramirez.civabusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivaBusApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivaBusApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salimramirez/civabusapi/CivaBusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salimramirez.civabusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivaBusApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
